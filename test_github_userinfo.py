d1ff65f2a04ee68664f27bd9a666b7cd33b2f4b6
ssaha30 <ssaha8@stevens.edu>
2026-02-16 13:08:08 -0500

    Update test_github_userinfo.py

--- test_github_userinfo.py
+++ test_github_userinfo.py
@@ -6,7 +6,6 @@ import github_userinfo
 #Test 1: Testing Program runs
 
 class TestGitHubInfo(unittest.TestCase):
-    
     def test_github_userinfo_runs(self):
         user = "richkempinski"
         github_userinfo.githubrepo(user)
@@ -15,9 +14,9 @@ class TestGitHubInfo(unittest.TestCase):
 # Test 2: Test if the user has any repositories
     def test_userrepos(self):
         user = "richkempinski"
-        url = "https://api.github.com/users/" + user + "/repos"
-        response = requests.get(url)
-        repos = json.loads(response.text)
+        repos_url = "https://api.github.com/users/" + user + "/repos"
+        repos_response = requests.get(repos_url)
+        repos = json.loads(repos_response.text)
         self.assertIsInstance(repos,list)
         self.assertGreater(len(repos),0)
 
@@ -26,14 +25,18 @@ class TestGitHubInfo(unittest.TestCase):
         user = "richkempinski"
         repo = "hellogitworld"
 
-        url = "https://api.github.com/repos/" + user + "/" + repo + "/commits"
-        response = requests.get(url)
+        commits_url = "https://api.github.com/repos/" + user + "/" + repo + "/commits"
+        commits_response = requests.get(commits_url)
 
-        commits = json.loads(response.text)
+        commits = json.loads(commits_response.text)
 
         self.assertIsInstance(commits, list)
         self.assertGreaterEqual(len(commits), 0)
 
-    
-
-
+#Test 4: Test by inputting the user has putting 0 repos
+    def test_forrepos(self):
+        user = "richkempinski"
+        url = "https://api.github.com/users/" + user + "/repos"
+        response = requests.get(url)
+        repos = json.loads(response.text)
+        self.assertEqual(len(repos),0)
